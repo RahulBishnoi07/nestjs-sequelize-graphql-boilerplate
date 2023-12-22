6fff70d986092044061d800cadaab5612a682778
Rahul Bishnoi <rahul.gdr29@gmail.com>
2023-12-22 13:58:40 +0530

    Db connection and .nvmrc added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Dialect } from 'sequelize/types';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { applicationConfig } from 'config';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: applicationConfig.db.db_dialect as Dialect,
+      host: applicationConfig.db.host,
+      username: applicationConfig.db.user,
+      password: applicationConfig.db.password,
+      port: parseInt(applicationConfig.db.port, 10),
+      database: applicationConfig.db.name,
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
